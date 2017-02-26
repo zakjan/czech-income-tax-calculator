3825d2d5b831bda566856701929de698626daf86
Jan Zak <zj@zakjan.cz>
2017-02-26 18:14:36 +0100

    Add maximal social insurance. Closes #3

--- src/services/taxCalculator.js
+++ src/services/taxCalculator.js
@@ -9,16 +9,41 @@ const employerHealthInsuranceRate = 0.09;
 const incomeTaxDeductionForPayer = 24840;
 const minimalSocialInsuranceTaxableIncome = 77652;
 const minimalHealthInsuranceTaxableIncome = 155304;
+const maximalSocialInsuranceTaxableIncome = 1355136;
+
 
 const TaxCalculator = {
-  taxableIncomeFromIncomeAndExpense: (income, expense) => Math.floor(Math.max(income - expense, 0) / 100) * 100,
-  incomeTaxFromTaxableIncome: (taxableIncome) => Math.max(taxableIncome * incomeTaxRate - incomeTaxDeductionForPayer, 0),
-  socialInsuranceFromTaxableIncome: (taxableIncome) => Math.max(taxableIncome / 2, minimalSocialInsuranceTaxableIncome) * socialInsuranceRate,
-  healthInsuranceFromTaxableIncome: (taxableIncome) => Math.max(taxableIncome / 2, minimalHealthInsuranceTaxableIncome) * healthInsuranceRate,
-  employeeSocialInsuranceFromWage: (wage) => wage * employeeSocialInsuranceRate,
-  employeeHealthInsuranceFromWage: (wage) => wage * employeeHealthInsuranceRate,
-  employerSocialInsuranceFromWage: (wage) => wage * employerSocialInsuranceRate,
-  employerHealthInsuranceFromWage: (wage) => wage * employerHealthInsuranceRate,
+  taxableIncomeFromIncomeAndExpense: (income, expense) => {
+    return Math.floor(Math.max(income - expense, 0) / 100) * 100;
+  },
+
+  incomeTaxFromTaxableIncome: (taxableIncome) => {
+    return Math.max(taxableIncome * incomeTaxRate - incomeTaxDeductionForPayer, 0);
+  },
+
+  socialInsuranceFromTaxableIncome: (taxableIncome) => {
+    return Math.min(Math.max(taxableIncome / 2, minimalSocialInsuranceTaxableIncome), maximalSocialInsuranceTaxableIncome) * socialInsuranceRate;
+  },
+
+  healthInsuranceFromTaxableIncome: (taxableIncome) => {
+    return Math.max(taxableIncome / 2, minimalHealthInsuranceTaxableIncome) * healthInsuranceRate;
+  },
+
+  employeeSocialInsuranceFromWage: (wage) => {
+    return wage * employeeSocialInsuranceRate;
+  },
+
+  employeeHealthInsuranceFromWage: (wage) => {
+    return wage * employeeHealthInsuranceRate;
+  },
+
+  employerSocialInsuranceFromWage: (wage) => {
+    return wage * employerSocialInsuranceRate;
+  },
+
+  employerHealthInsuranceFromWage: (wage) => {
+    return wage * employerHealthInsuranceRate;
+  },
 };
 
 
