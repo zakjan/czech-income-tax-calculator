9a8ddd4947e2be2446126bb9c2fedcd9f016ca19
Jan Zak <zj@zakjan.cz>
2018-02-04 23:21:32 +0100

    Redirect to new domain

--- web.js
+++ web.js
@@ -3,6 +3,13 @@ var express = require('express');
 var port = process.env.PORT || 8080;
 
 var app = express();
+app.use((req, res, next) => {
+  var host = req.get('Host');
+  if (host === 'czech-income-tax-calculator.zakjan.cz') {
+    return res.redirect(301, 'http://tax.zakjan.cz/' + req.originalUrl);
+  }
+  return next();
+});
 app.use(express.static('dist'));
 app.listen(port);
 
