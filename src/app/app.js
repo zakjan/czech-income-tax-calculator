4747aae526a108f845262c9503c0ba7aa0c00157
Jan Zak <zj@zakjan.cz>
2022-05-03 23:23:59 +0200

    Update Github corner

--- src/app/app.js
+++ src/app/app.js
@@ -13,12 +13,6 @@ const App = props => {
       <h1>Kalkulačka daně z příjmů za rok {TaxCalculator.getYear()}</h1>
       <AppForm {...props} />
       <TaxPayerTab {...props} />
-
-      <p>
-        <a className="github-button" href="https://github.com/zakjan/czech-income-tax-calculator" data-icon="octicon-star" data-show-count="true" aria-label="Star zakjan/czech-income-tax-calculator on GitHub">Star</a>
-        {' '}
-        <a className="github-button" href="https://github.com/sponsors/zakjan" data-icon="octicon-heart" aria-label="Sponsor @zakjan on GitHub">Sponsor</a>
-      </p>
     </div>
   );
 };
