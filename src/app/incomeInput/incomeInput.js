494162fb60a8bb4e6d8fe15cf08a7b2dbee1f005
inson1 <75314629+inson1@users.noreply.github.com>
2023-10-27 20:31:37 +0200

    Done

--- src/app/incomeInput/incomeInput.js
+++ src/app/incomeInput/incomeInput.js
@@ -27,7 +27,7 @@ class IncomeInput extends React.Component {
 
     return (
       <div className="income-input">
-        <p><MoneyInput value={income} onChange={this.props.onChange.bind(this)} /></p>
+        <p><MoneyInput value={income} onChange={this.props.onChange.bind(this)} /> / rok</p>
         <p><MoneyInput value={incomePerMonth} onChange={this.onChangeIncomePerMonth.bind(this)} /> / měsíc</p>
         <p><MoneyInput value={incomePerDay} onChange={this.onChangeIncomePerDay.bind(this)} /> / den</p>
         <p><MoneyInput value={incomePerHour} onChange={this.onChangeIncomePerHour.bind(this)} /> / hodina</p>
